c0cde2f203ca3c233075069833e6d5a3a8c43f92
00Programmer00 <dahnenko02501@gmail.com>
2017-09-18 15:16:18 +0300

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-// import './App.css';
+
 import TestListComponent from './components/TestListComponent.js';
 
 class App extends Component {
