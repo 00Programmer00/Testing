568ad55c4215fc4326c83f70954b637d81c3e455
taras90ck <taras90ck@gmail.com>
2017-09-20 12:29:18 +0300

    answer variants displayed

--- src/components/MultipleAnswerComponent.js
+++ src/components/MultipleAnswerComponent.js
@@ -2,40 +2,21 @@ import React from 'react';
 
 export default class MultipleAnswerComponent extends React.Component{
     render(){
+        let variants = this.props.question.answerVariants.map((variant, i) => {
+          return (
+            <div className="form-check">
+                <label className="form-check-label">
+                    <input className="form-check-input" type="checkbox" value={"option" + i} />
+                    {variant.title}
+                </label>
+            </div>
+          )
+        })  
         return(
             <form>
                 <div className="form-check">
                     <label>Тип ответа выбор нескольких вариантов из предложенных</label>
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input className="form-check-input" type="checkbox" />
-                            Option 1
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input className="form-check-input" type="checkbox" />
-                            Option 2
-                        </label>
-                    </div>
-                    <div className="form-check">
-                        <label className="form-check-label">
-                            <input className="form-check-input" type="checkbox" />
-                            Option 3
-                        </label>
-                        <div className="form-check">
-                            <label className="form-check-label">
-                                <input className="form-check-input" type="checkbox" />
-                                Option 4
-                            </label>
-                        </div>
-                        <div className="form-check">
-                            <label className="form-check-label">
-                                <input className="form-check-input" type="checkbox" />
-                                Option 5
-                            </label>
-                        </div>
-                    </div>
+                    {variants}
                 </div>
             </form>
         )
